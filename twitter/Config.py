360edf04c3d7099ea239fe01869b2986963342fa
Kate <xunwanwang@gmail.com>
2018-04-18 13:47:51 +1000

    steamListener template and config file

---
+++ twitter/Config.py
@@ -0,0 +1,8 @@
+class Config:
+    # Consumer Key/Secret (Needed for Streamer)
+    consumer_key = "dW8ITv7LBUo3wC0vmyFqNUp03"
+    consumer_secret = "UcVTOquHkQ4qrU4y7QE1Eq6kkACavJvh9FBGCUd2YmTYJtFT3Z"
+
+    # Access Token/Secret (Needed for Streamer)
+    access_token = "1369762718-qYPh0KF8AcsxD0djnU2iAbpUYRd0uvXHyr9vcST"
+    access_token_secret = "oXoqBy4DOM93fBqHxoenalXZt5wKr8MIO8DdOnLSVpPno"
